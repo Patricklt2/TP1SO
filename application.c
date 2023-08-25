22a5daf2932db5f16c4503daed4f232048f57d35
Patricklt2 <ptorlaschis@gmail.com>
2023-08-25 16:02:47 -0300

    includes  y algunos defines

--- application.c
+++ application.c
@@ -1,8 +1,13 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define SLAVENUM 5
+#define MAXFILESPERSLAVE 2
 
-int main(int argc, char *argv) {
+int main(int argc, char *argv[]) {
 
-    int fAmount = argc - 1;
-    int currentFile = 1;
+    int currentFile = 0;
 
     //creates a buffer and prints the information necessary in stdout for the vista process to connect
 
@@ -13,7 +18,7 @@ int main(int argc, char *argv) {
     //waits until a slave process finishes, and writes the result on the buffer
 
     //assigns a new file to the slave process, if there's none left, the slave is killed
-    if(currentFile >= fAmount) {
+    if(currentFile > MAXFILESPERSLAVE) {
         //kills
     } else {
         //gives the slave a new file
