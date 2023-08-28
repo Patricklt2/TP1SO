0ed8103c2fb2b9ed7be49fe0dd86a756f34033a8
Tim S <84805291+tm-sm@users.noreply.github.com>
2023-08-28 12:49:11 -0300

    barebones implementation of createSlaveProcesses in app.c

--- application.c
+++ application.c
@@ -2,11 +2,13 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 #define SLAVENUM 5
-#define MAXFILESPERSLAVE 2
+#define MAXFILESPERSLAVE 20
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *files[]) {
 
+    int fAmount = argc - 1;
     int currentFile = 0;
 
     //creates a buffer and prints the information necessary in stdout for the vista process to connect
@@ -14,6 +16,8 @@ int main(int argc, char *argv[]) {
     //waits 2 seconds for a vista process
 
     //decides how many slave processes need to be created and initializes them
+
+    pid_t* slaveProcesses = createSlaveProcesses(fAmount);
 
     //waits until a slave process finishes, and writes the result on the buffer
 
@@ -25,4 +29,18 @@ int main(int argc, char *argv[]) {
     }
 
     //once all the slaves finish, writes to the result file and returns
+}
+
+
+pid_t* createSlaveProcesses(int fAmount) {
+    //creates as many slaves as files if the number is too low, creates x slaves per y files (eg. 5 slaves every 20 files)
+    int slaves = (fAmount < SLAVENUM) ? (fAmount) : ((fAmount / MAXFILESPERSLAVE) * SLAVENUM);
+
+    pid_t* slaveArr = malloc(slaves * sizeof(pid_t));
+
+    for(int i=0; i<slaves; i++) {
+        //creates slaves and stores them in the array
+    }
+
+    return slaveArr;
 }
