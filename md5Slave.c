04403ebd98d37e68a5816fc13b048fedf5387321
Patricklt2 <ptorlaschis@gmail.com>
2023-09-07 11:02:27 -0300

    Slave toma input de STDIN en loop infinito, espera a ser killed por el padre. Md5Slave ahora imprime bien el nombre del archivo

--- md5Slave.c
+++ md5Slave.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libgen.h>
 
 #define BYTES_MD5HASH 33 //32 para el hash + 1 para el  '\0'
 
-int main(int argc, const char * argv[]) {
+int main(int argc, char * argv[]) {
+    if(argc != 1){
+        exit(1);
+    }
     // comando para calcular el md5
     char *command = malloc(sizeof(char)*100);
     strcpy(command,"md5sum ");
@@ -14,19 +18,18 @@ int main(int argc, const char * argv[]) {
     FILE *fp = popen(command, "r");
     if (fp == NULL) {
         perror("Error executing command");
-        return 1;
+        exit(1);
     }
 
     // Leo y cargo el hash
     char buffer[BYTES_MD5HASH];
     fgets(buffer, sizeof(buffer), fp);
 
-    printf("%s %s",buffer, argv[0]);
-
+    printf("%s %s",buffer, basename(argv[0]));
 
     // Cierro pipe
     pclose(fp);
     free(command);
 
-    return 0;
+    exit(0);
 }
--- slave.c
+++ slave.c
@@ -4,7 +4,9 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define NUM_CHILD 3
+#define NUM_CHILD 20
+#define TRUE 1
+#define FALSE 0
 
 void child_process(int pipe_fd_read, int pipe_fd_write, char * fPath) {
     // Cierro pipe de lectura
@@ -16,48 +18,59 @@ void child_process(int pipe_fd_read, int pipe_fd_write, char * fPath) {
     // Proceso nuevo
     char * argv[] = {fPath,NULL};  // Le paso el path al hijo
     char * const envp[] = {NULL};
-    execve("./md5Slave", argv, envp);
+    execve("./md5Slave.out", argv, envp);
 
     perror("Error executing child program");
     exit(1);
 }
 
+//Uso argv[0] como write fd (para el pipe) y argv[1] como read fd (para el pipe)
+//El read and write fd los necesito para cerrar los pipes a los md5Slave
 int main() {
+
     pid_t child_pids[NUM_CHILD];
-    int pipe_fds[NUM_CHILD][2];  // Array de fd's
+    int pipe_fds[NUM_CHILD][2];  // Array de fd's para el md5Slave
 
-    char *fPaths[NUM_CHILD] = {
-            "./README.txt",
-            "./vista.c",
-            "./application.c"
-    };  // Paths to files
 
-    for (int i = 0; i < NUM_CHILD; i++) {
+    int i = 0;
+    char bufferWrite[128];  //Write to parent
+    char bufferRead[128];   //Read from parent
+
+    while(TRUE){
+
         if (pipe(pipe_fds[i]) == -1) {
             perror("Error creating pipe");
-            return 1;
+            exit(1);
         }
+
+
+        ssize_t bytes_r = read(STDIN_FILENO,bufferRead,sizeof(bufferRead));
+        bufferRead[bytes_r] = '\0';
+
 
         child_pids[i] = fork();
 
         if (child_pids[i] == -1) {
             perror("Error creating child process");
-            return 1;
+            exit(1);
         } else if (child_pids[i] == 0) {  // hijo
-            child_process(pipe_fds[i][0], pipe_fds[i][1], fPaths[i]);
+            child_process(pipe_fds[i][0], pipe_fds[i][1], bufferRead);
             exit(0);
         } else {  // padre
+            close(pipe_fds[i][1]);
 
+            ssize_t bytes_read = read(pipe_fds[i][0],bufferWrite,sizeof(bufferWrite));
+            bufferWrite[bytes_read] = '\0';                                     //asegurar que el '\0' este bien
+            printf("%d %s\n",getpid(),bufferWrite);
+
+            close(pipe_fds[i][0]);
+            i++;
+
+            if(i == 20){  //Para ese entonces el proceso hijo 0 ya va a haber terminado, entonces puedo rehusar el fd
+                i = 0;
+            }
         }
     }
 
-    // Padre sigue // levanto los datos
-    char buff[128];
-    for (int i = 0; i < NUM_CHILD; i++) {
-       ssize_t bytes_read = read(pipe_fds[i][0],buff,sizeof(buff));
-       buff[bytes_read] = '\0';                                     //asegurar que el '\0' este bien
-        printf("%d %s\n",getpid(),buff);
-    }
-
-    return 0;
+    exit(0);
 }
