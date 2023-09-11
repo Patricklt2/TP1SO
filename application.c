00877c7f4db5e9bfd216e429c9602d1159fa17f9
Ncasella <nicolascasella02@gmail.com>
2023-09-11 11:05:59 -0300

    ah yess, progress

--- application.c
+++ application.c
@@ -35,8 +35,8 @@ int main(int argc, char *argv[]) {
         enqueue(q,argv[i]);
     }
 
-    int slavesNum = calculateSlavesNum(argc);//ver bien de como calcular la cant de slaves
-    pipechannels *pipes = malloc(slavesNum * (sizeof(pipechannels)));
+    int slavesNum = calculateSlavesNum(argc-2);//ver bien de como calcular la cant de slaves
+    pipechannels pipes[slavesNum];
     for(int i=0;i<slavesNum;i++){
         if(pipe(pipes[i].master_a_slave)==-1||pipe(pipes[i].slave_a_master)==-1){
             perror("pipe machine broke\n");
@@ -45,14 +45,15 @@ int main(int argc, char *argv[]) {
         if((pipes[i].pid=fork())==0){
             close(pipes[i].master_a_slave[1]);
             close(pipes[i].slave_a_master[0]);
-            dup2(pipes[i].slave_a_master[1],STDOUT_FILENO);
-            dup2(pipes[i].master_a_slave[0],STDIN_FILENO);
+            close(STDOUT_FILENO);
+            dup(pipes[i].slave_a_master[1]);
+            close(STDIN_FILENO);
+            dup(pipes[i].master_a_slave[0]);
             execve("./slave.out",NULL,NULL);
          }
             close(pipes[i].master_a_slave[0]);
             close(pipes[i].slave_a_master[1]);
     }
-    sleep(2);
     memoryADT mem = createSharedMem();
     fputs(getMemoryID(mem), stdout);
 
@@ -65,14 +66,15 @@ int main(int argc, char *argv[]) {
     int i=0;
     while(!isempty(q)){
         dequeue(q,buff);
-        write(pipes[i%slavesNum].master_a_slave[0],buff,sizeof(buff));
-        ssize_t len=read(pipes[i%slavesNum].slave_a_master[1],buffWrite,sizeof(buffWrite));
-        buffWrite[len]='\0';
-        printf("%s",buffWrite);
-        i++;
         sleep(1);
+        write(pipes[i%slavesNum].master_a_slave[1],buff,strlen(buff));
+        ssize_t len=read(pipes[i%slavesNum].slave_a_master[0],buffWrite,sizeof(buffWrite));
+        if(len<0){printf("error en read\n");exit(1);}
+        buffWrite[len]='\0';
+        printf("PID: %s",buffWrite);
+        i++;
     }
-
+    free(q);
 
 
 
@@ -98,8 +100,12 @@ int main(int argc, char *argv[]) {
     //once all the slaves finish, writes to the result file and returns
 
     setFlag(mem, 1);
+    for(int i=0;i<slavesNum;i++){
+        close(pipes[i].master_a_slave[1]);
+        close(pipes[i].slave_a_master[0]);
+        kill(pipes[i].pid,1);
+    }
 
-    free(pipes);
 }
 
 
